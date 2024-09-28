bba59af41a954787dcecf4ff29649fe5f4189fa5
yntymak <kubanychevyntymak21@gmail.com>
2024-09-28 17:28:11 +0600

    header fix

--- src/logic/services/clients/pages/Header/Header.tsx
+++ src/logic/services/clients/pages/Header/Header.tsx
@@ -4,17 +4,19 @@ const Header = () => {
   return (
     <div>
       <div className={s.header}>
-        <div className={s.head_home}>
-          <div className={s.head}>
-            <h1>Home</h1>
-            <h1>Regions</h1>
-            <h1>Gallery</h1>
-            <h1>Routes</h1>
-            <h1>Culture</h1>
-          </div>
-          <div className={s.btn_home}>
-            <button className={s.header_btn}>Eng</button>
-            <button className={s.header_btn_two}>Sign Up</button>
+        <div className="container">
+          <div className={s.head_home}>
+            <div className={s.head}>
+              <h1>Home</h1>
+              <h1>Regions</h1>
+              <h1>Gallery</h1>
+              <h1>Routes</h1>
+              <h1>Culture</h1>
+            </div>
+            <div className={s.btn_home}>
+              <button className={s.header_btn}>Eng</button>
+              <button className={s.header_btn_two}>Sign Up</button>
+            </div>
           </div>
         </div>
       </div>
