eb7639a679d3e8cae55c52971795ace0d3aeeeb7
yntymak <kubanychevyntymak21@gmail.com>
2024-09-29 13:28:47 +0600

    section/home finish

--- src/logic/services/clients/pages/static/home/Welcome/Welcome.tsx
+++ src/logic/services/clients/pages/static/home/Welcome/Welcome.tsx
@@ -1,7 +1,67 @@
+import Image from "next/image";
 import React from "react";
+import secImg from "@/logic/services/clients/pages/static/home/img/image 989.png";
+import s from "./Welcome.module.scss";
+import a from "./img/instagram.png";
+import b from "./img/f_logo_RGB-Blue_512 1 (2).png";
+import c from "./img/VK Logo _ Main (1).png";
+import d from "./img/Mail _ undefined (2).png";
+import { Search } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 
 const Welcome = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className={s.secHome}>
+        <div className="container">
+          <div className={s.secImg}>
+            <Image
+              src={secImg}
+              alt="img"
+              quality={70}
+              width={1579}
+              height={730}
+            />
+            <div className={s.imageText}>
+              <div className={s.box}>
+                <div className={s.secInp}>
+                  <h1>Welcome to the amazing Kyrgyzstan!</h1>
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <div className={s.search}>
+                    <Search className={s.searchIcon} />
+                    <input placeholder="Where to go?" />
+                    <div className={s.btn_inp}>
+                      <ArrowUpRight className={s.downo} />
+                      <button>ㅤㅤㅤㅤㅤㅤ</button>
+                    </div>
+                  </div>
+                  <br />
+                  <br />
+                  <p>
+                    Are you ready to embark on an exciting journey through the{" "}
+                    <br />
+                    stunning country of Central Asia? We are ready to help you
+                    plan <br /> the perfect trip to Kyrgyzstan.
+                  </p>
+
+                  <div className={s.block}></div>
+                </div>
+              </div>
+              <div className={s.icon}>
+                <Image src={a} alt="img" quality={70} width={24} height={24} />
+                <Image src={b} alt="img" quality={70} width={24} height={24} />
+                <Image src={c} alt="img" quality={70} width={24} height={24} />
+                <Image src={d} alt="img" quality={70} width={24} height={24} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
